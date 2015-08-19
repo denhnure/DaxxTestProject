8f988b003beafa54eae5280e25ef382f1f2c99bf
denhnure <denhnure@gmail.com>
2015-08-20 01:27:07 +0300

    Creating the Data Model

---
+++ DaxxTestProject/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DaxxTestProject.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public bool AgreeToWorkForFood { get; set; }
+        public int CountryId { get; set; }
+        public int StateId { get; set; }
+        public virtual Country Country { get; set; }
+        public virtual State State { get; set; }
+    }
+}
