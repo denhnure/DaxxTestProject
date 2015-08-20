79c29cb8bb419ba2f297ff485d90735c04d62bea
denhnure <denhnure@gmail.com>
2015-08-20 07:16:16 +0300

    Setting up EF to use a SQL Server Express LocalDB database

--- DaxxTestProject/DAL/RegistrationContext.cs
+++ DaxxTestProject/DAL/RegistrationContext.cs
@@ -5,6 +5,11 @@ namespace DaxxTestProject.DAL
 {
     public class RegistrationContext : DbContext
     {
+        public RegistrationContext()
+            : base("RegistrationContext")
+        {
+        }
+
         public DbSet<User> Users { get; set; }
         public DbSet<Country> Countries { get; set; }
         public DbSet<State> States { get; set; }
