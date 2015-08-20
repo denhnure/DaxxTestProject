15a4d6a75d209fd542b77ac8cf0bb3e577eba82c
denhnure <denhnure@gmail.com>
2015-08-20 08:04:13 +0300

    running add-migration InitialCreate command

---
+++ DaxxTestProject/Migrations/201508200459095_InitialCreate.cs
@@ -0,0 +1,63 @@
+namespace DaxxTestProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Country",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.State",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        CountryId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Country", t => t.CountryId, cascadeDelete: true)
+                .Index(t => t.CountryId);
+            
+            CreateTable(
+                "dbo.User",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Login = c.String(),
+                        Password = c.String(),
+                        AgreeToWorkForFood = c.Boolean(nullable: false),
+                        CountryId = c.Int(nullable: false),
+                        StateId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Country", t => t.CountryId, cascadeDelete: true)
+                .ForeignKey("dbo.State", t => t.StateId, cascadeDelete: true)
+                .Index(t => t.CountryId)
+                .Index(t => t.StateId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.User", "StateId", "dbo.State");
+            DropForeignKey("dbo.User", "CountryId", "dbo.Country");
+            DropForeignKey("dbo.State", "CountryId", "dbo.Country");
+            DropIndex("dbo.User", new[] { "StateId" });
+            DropIndex("dbo.User", new[] { "CountryId" });
+            DropIndex("dbo.State", new[] { "CountryId" });
+            DropTable("dbo.User");
+            DropTable("dbo.State");
+            DropTable("dbo.Country");
+        }
+    }
+}
